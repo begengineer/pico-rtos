527500b07ed5ae172642f8a224c8a252b88f2b0f
YukiTanaka <anpanma6@gmail.com>
2025-12-30 13:24:09 +0900

    create ignore file

---
+++ kernel/scheduler.c
@@ -0,0 +1,38 @@
+﻿/* 
+ *** Try Kernel
+ *      スケジューラ
+*/
+#include <trykernel.h>
+#include <knldef.h>
+
+TCB *ready_queue[CNF_MAX_TSKPRI];    // タスクのレディキュー
+TCB *cur_task;                       // 実行中のタスク
+TCB *sche_task;                      // 次に実行するタスク
+
+UW  disp_running;                    // ディスパッチャ実行中
+
+/* タスクのスケジューリング */
+void scheduler(void)
+{
+    INT		i;
+
+    for(i = 0; i < CNF_MAX_TSKPRI; i++) {
+        if( ready_queue[i] != NULL) break;
+    }
+
+    if(i < CNF_MAX_TSKPRI) {
+        sche_task = ready_queue[i];
+    } else {
+        sche_task = NULL;    // 実行できるタスクは無い
+    }
+
+    // 追加コード
+    // タスク
+    if(sche_task != cur_task && sche_task != NULL) {
+        tm_putstring("[SCHED] Task Switch\n");
+    }
+
+    if(sche_task != cur_task && !disp_running) {
+        dispatch();         // ディスパッチャを実行
+    }
+}
