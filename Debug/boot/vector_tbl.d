3c81afa94471e6fab05393076658e1334a2da762
YukiTanaka <anpanma6@gmail.com>
2025-12-30 13:35:46 +0900

    Fix .gitignore to include Debug folder with makefiles

---
+++ Debug/boot/vector_tbl.d
@@ -0,0 +1,9 @@
+boot/vector_tbl.o: ../boot/vector_tbl.c \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h
+C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h:
