d9df4d6f95f4da4d2d8833afcbdbed428f1016cd
YukiTanaka <anpanma6@gmail.com>
2025-12-30 13:23:47 +0900

    create ignore file

--- Debug/kernel/context.d
+++
@@ -1,9 +0,0 @@
-kernel/context.o: ../kernel/context.c \
- C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h \
- C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h \
- C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h \
- C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h
-C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h:
-C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h:
-C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h:
-C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h:
