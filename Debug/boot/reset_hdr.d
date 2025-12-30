1d3898b4da3094b7d607ff06abb1009789ca808c
YukiTanaka <anpanma6@gmail.com>
2025-12-30 13:16:21 +0900

    initial commit

---
+++ Debug/boot/reset_hdr.d
@@ -0,0 +1,9 @@
+boot/reset_hdr.o: ../boot/reset_hdr.c \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h \
+ C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h
+C:\Users\anpan\eclipse-workspace\RTOS\include/typedef.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/sysdef.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/syslib.h:
+C:\Users\anpan\eclipse-workspace\RTOS\include/knldef.h:
---
+++ Debug/kernel/subdir.mk
@@ -0,0 +1,65 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../kernel/context.c \
+../kernel/eventflag.c \
+../kernel/inittsk.c \
+../kernel/scheduler.c \
+../kernel/semaphore.c \
+../kernel/syslib.c \
+../kernel/systimer.c \
+../kernel/task_mange.c \
+../kernel/task_queue.c \
+../kernel/task_sync.c 
+
+S_UPPER_SRCS += \
+../kernel/dispatch.S 
+
+OBJS += \
+./kernel/context.o \
+./kernel/dispatch.o \
+./kernel/eventflag.o \
+./kernel/inittsk.o \
+./kernel/scheduler.o \
+./kernel/semaphore.o \
+./kernel/syslib.o \
+./kernel/systimer.o \
+./kernel/task_mange.o \
+./kernel/task_queue.o \
+./kernel/task_sync.o 
+
+S_UPPER_DEPS += \
+./kernel/dispatch.d 
+
+C_DEPS += \
+./kernel/context.d \
+./kernel/eventflag.d \
+./kernel/inittsk.d \
+./kernel/scheduler.d \
+./kernel/semaphore.d \
+./kernel/syslib.d \
+./kernel/systimer.d \
+./kernel/task_mange.d \
+./kernel/task_queue.d \
+./kernel/task_sync.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+kernel/%.o: ../kernel/%.c kernel/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -ffreestanding -g3 -I"C:\Users\anpan\eclipse-workspace\RTOS\include" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+kernel/%.o: ../kernel/%.S kernel/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -ffreestanding -g3 -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
